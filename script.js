3c817cca70b276f8f236913ac1e95711939f2a6d
Tejas <tdmundhe32@gmail.com>
2024-02-09 15:29:40 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let num = prompt("Enter the number to find it's Factorial => ")
+
+let Factorial=1;
+
+for(let i=1 ;i<=num ; i++){
+    Factorial *=i;
+}
+
+alert("The factorial of " + num + " is " + Factorial);
